8cabbd276778af379a342f1c8c39d49864d2e02e
Mazcho <putrantonicholaus@gmail.com>
2024-01-06 05:07:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,172 +22,180 @@ st.set_page_config(
 
 #========= Load MOdel ==========================
 # Load the pre-trained model
-st.title("Hungarian Heart Disease")
-
-algoritma_terpilih = st.selectbox("Pilih Algoritma", options=["K-NN", "RandomForest", "xGBoost"])
-
-if algoritma_terpilih == "K-NN":
-    with open('knnmodel.pkl', 'rb') as file:
-        model = pickle.load(file)
-
-elif algoritma_terpilih == "RandomForest":
-    with open('randomforest_Oversampled_normalisasi.pkl', 'rb') as file:
-        model = pickle.load(file)
-
-elif algoritma_terpilih == "xGBoost":
-    with open('xgBoost_tuning.pkl', 'rb') as file:
-        model = pickle.load(file)
+with st.sidebar:
+    menu = st.radio(
+        "Chhose Menu",
+        ("Home", "App")
+    )
 
 
-#========== Load Data Set ======================
-df = pd.read_csv("Hungarian_Data.csv")
-#===============================================
+if menu == "App":
+    st.title("Hungarian Heart Disease")
 
-#========== Oversampling Data Set ==============
-df_copy = df.copy()
+    algoritma_terpilih = st.selectbox("Pilih Algoritma", options=["K-NN", "RandomForest", "xGBoost"])
 
-featured = df_copy.drop("target",axis=1)
-target = df_copy["target"]
+    if algoritma_terpilih == "K-NN":
+        with open('knnmodel.pkl', 'rb') as file:
+            model = pickle.load(file)
 
-smote = SMOTE(random_state=42)
-featured_smote_resampled, target_smote_resampled = smote.fit_resample(featured, target)
+    elif algoritma_terpilih == "RandomForest":
+        with open('randomforest_Oversampled_normalisasi.pkl', 'rb') as file:
+            model = pickle.load(file)
 
-normalisasiminmax = MinMaxScaler()
-featured_smote_resampled_normalisasi = normalisasiminmax.fit_transform(featured_smote_resampled)
-
-X_train, X_test, y_train, y_test = train_test_split(featured_smote_resampled_normalisasi, target_smote_resampled, test_size=0.2, random_state=42,stratify = target_smote_resampled)
-
-# hasil prediksi model dalam %
-y_pred = model.predict(X_test)
-
-# kalkuliasi akurasi
-accuracy = accuracy_score(y_test, y_pred)
+    elif algoritma_terpilih == "xGBoost":
+        with open('xgBoost_tuning.pkl', 'rb') as file:
+            model = pickle.load(file)
 
 
-# Display the accuracy
-st.write(f"Accuracy dari pickel: ",round(accuracy*100,1))
+    #========== Load Data Set ======================
+    df = pd.read_csv("Hungarian_Data.csv")
+    #===============================================
 
-# ===================================================
+    #========== Oversampling Data Set ==============
+    df_copy = df.copy()
 
-# Assume min_max_values.json contains the minimum and maximum values for normalization
-with open("min_max_values.json", "r") as json_file:
-    min_max_values = json.load(json_file)
+    featured = df_copy.drop("target",axis=1)
+    target = df_copy["target"]
 
-tab1,tab2 = st.tabs(["Single Predixt","Multi Predict"])
+    smote = SMOTE(random_state=42)
+    featured_smote_resampled, target_smote_resampled = smote.fit_resample(featured, target)
 
-with tab1:
-    col1,col2 = st.columns(2)
-# membuat kolom inputan untuk single prediksi
-    #kolom umur
-    with col1:
-        age = st.number_input("Age")
+    normalisasiminmax = MinMaxScaler()
+    featured_smote_resampled_normalisasi = normalisasiminmax.fit_transform(featured_smote_resampled)
 
-        #kolom sex, 1 untuk cowok 0 untuk cewek
-        sex = int(st.selectbox("Sex", options=["Male", "Female"]) == "Male")
+    X_train, X_test, y_train, y_test = train_test_split(featured_smote_resampled_normalisasi, target_smote_resampled, test_size=0.2, random_state=42,stratify = target_smote_resampled)
 
-        #Chest pain tipenya
-        cp_options = {
-            1 : "Typical angina",
-            2 : "Atypical angina",
-            3 : "Non-anginal pain",
-            4 : "Asymptomatic"
-        }
-        cp = st.selectbox("Chest Pain Type", options=list(cp_options.keys()), format_func=lambda x: cp_options[x])
-        selected_value = cp_options[cp]
+    # hasil prediksi model dalam %
+    y_pred = model.predict(X_test)
 
-        #inputan untuk kolom trestbps
-        trestbps = st.number_input("Trestbps mm Hg")
+    # kalkuliasi akurasi
+    accuracy = accuracy_score(y_test, y_pred)
 
-        #inputan untuk kolom chol
-        chol = st.number_input("Serum Cholestoral in mg/dl")
 
-    with col2:
-        #inputan untuk fbs dengan urutan sesuai index
-        fbs = int(st.selectbox("Fasting Blood Sugar > 120 mg/dl", options=["True", "False"]) == "True")
+    # Display the accuracy
+    st.write(f"Accuracy dari pickel: ",round(accuracy*100,1))
 
-        restecg = ["Normal", "St-T Wave Abnormality", "Left Ventricular Hypertrophy"].index(
-            st.selectbox("Resting Electrocardiographic Results", ["Normal", "St-T Wave Abnormality", "Left Ventricular Hypertrophy"])
-        )
+    # ===================================================
 
-        # inputan thalac
-        thalac = st.number_input("Maximum Heart Rate Achieved")
+    # Assume min_max_values.json contains the minimum and maximum values for normalization
+    with open("min_max_values.json", "r") as json_file:
+        min_max_values = json.load(json_file)
 
-        #inputan exan
-        exang = int(st.selectbox("Exercise Induced Angina", options=["No", "Yes"]) == "Yes")
+    tab1,tab2 = st.tabs(["Single Predixt","Multi Predict"])
 
-        #inputan oldpeak
-        oldpeak = st.number_input("OldPeak")
+    with tab1:
+        col1,col2 = st.columns(2)
+    # membuat kolom inputan untuk single prediksi
+        #kolom umur
+        with col1:
+            age = st.number_input("Age")
 
-    #memb uat tombol prediksi
-    predict_button = st.button("Predict", type="primary")
-    result = "-"
+            #kolom sex, 1 untuk cowok 0 untuk cewek
+            sex = int(st.selectbox("Sex", options=["Male", "Female"]) == "Male")
 
-    #membuat data input menjadi dataframe
-    data_input = [[age, sex, cp, trestbps, chol, fbs, restecg, thalac, exang, oldpeak]]
-    datauser = pd.DataFrame(data_input, columns=["Age", "Sex", "ChestPainType", "Trestbps", "Chol", "Fbs", "Restecg", "Thalac", "Exang", "OldPeak"])
-    
-    # Normalize the inputs based on min_max_values
-    normalized_inputs = [
-    (value - min_max_values[key]["min"]) / (min_max_values[key]["max"] - min_max_values[key]["min"])
-    for key, value in zip(min_max_values.keys(), [age, sex, cp, trestbps, chol, fbs, restecg, thalac, exang, oldpeak])
-    ]
+            #Chest pain tipenya
+            cp_options = {
+                1 : "Typical angina",
+                2 : "Atypical angina",
+                3 : "Non-anginal pain",
+                4 : "Asymptomatic"
+            }
+            cp = st.selectbox("Chest Pain Type", options=list(cp_options.keys()), format_func=lambda x: cp_options[x])
+            selected_value = cp_options[cp]
 
-    # Reshape the normalized inputs into a DataFrame
-    datauser_normalized = pd.DataFrame([normalized_inputs], columns=["Age", "Sex", "ChestPainType", "Trestbps", "Chol", "Fbs", "Restecg", "Thalac", "Exang", "OldPeak"])
-    
-    #ini untuk triger ketika tombol predik dipencet
-    if predict_button:
-        st.write(datauser)
-        # Now, you can use normalized_inputs for prediction
-        prediction = model.predict(datauser_normalized)
+            #inputan untuk kolom trestbps
+            trestbps = st.number_input("Trestbps mm Hg")
 
-        if prediction[0] == 0:
-            st.success("👍 You are Healthy")
-        else:
-            st.error(" 💔 You are diagnosed with heart disease")
+            #inputan untuk kolom chol
+            chol = st.number_input("Serum Cholestoral in mg/dl")
 
-with tab2:
-    #ini untuk multi prediksi
-    #upload csv
-    uploaded_df = st.file_uploader("Upload a CSV file", type='csv')
+        with col2:
+            #inputan untuk fbs dengan urutan sesuai index
+            fbs = int(st.selectbox("Fasting Blood Sugar > 120 mg/dl", options=["True", "False"]) == "True")
 
-# Normalisasi input di dalam tab 2
-    if uploaded_df:
-        # Menggunakan st.file_uploader untuk mengunggah file CSV
-        uploaded_df = pd.read_csv(uploaded_df)
+            restecg = ["Normal", "St-T Wave Abnormality", "Left Ventricular Hypertrophy"].index(
+                st.selectbox("Resting Electrocardiographic Results", ["Normal", "St-T Wave Abnormality", "Left Ventricular Hypertrophy"])
+            )
 
-        # Load JSON values from file
-        with open("min_max_values.json", "r") as json_file:
-            min_max_values_json = json.load(json_file)
+            # inputan thalac
+            thalac = st.number_input("Maximum Heart Rate Achieved")
 
-        # Ensure that the DataFrame is not empty before attempting to iterate
-        if not uploaded_df.empty:
-            # Iterate through each column in the uploaded DataFrame
-            for column in uploaded_df.columns:
-                # Check if the column is present in min_max_values_json
-                if column in min_max_values_json:
-                    # Normalize the column values
-                    uploaded_df[column] = (uploaded_df[column] - min_max_values_json[column]["min"]) / (min_max_values_json[column]["max"] - min_max_values_json[column]["min"])
+            #inputan exan
+            exang = int(st.selectbox("Exercise Induced Angina", options=["No", "Yes"]) == "Yes")
 
-            # Convert specific columns to float64
-            float_columns = ["trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak"]
-            uploaded_df[float_columns] = uploaded_df[float_columns].astype(float)
+            #inputan oldpeak
+            oldpeak = st.number_input("OldPeak")
 
-            # Membuat DataFrame untuk hasil prediksi
-            result_df = pd.DataFrame(columns=["Prediction Value", "Prediction Result"])
+        #memb uat tombol prediksi
+        predict_button = st.button("Predict", type="primary")
+        result = "-"
 
-            # Iterasi melalui setiap baris di DataFrame yang diunggah
-            for index, row in uploaded_df.iterrows():
-                # Buat DataFrame untuk satu baris hasil prediksi
-                data_tab2_normalized = pd.DataFrame([row], columns=uploaded_df.columns)
+        #membuat data input menjadi dataframe
+        data_input = [[age, sex, cp, trestbps, chol, fbs, restecg, thalac, exang, oldpeak]]
+        datauser = pd.DataFrame(data_input, columns=["Age", "Sex", "ChestPainType", "Trestbps", "Chol", "Fbs", "Restecg", "Thalac", "Exang", "OldPeak"])
+        
+        # Normalize the inputs based on min_max_values
+        normalized_inputs = [
+        (value - min_max_values[key]["min"]) / (min_max_values[key]["max"] - min_max_values[key]["min"])
+        for key, value in zip(min_max_values.keys(), [age, sex, cp, trestbps, chol, fbs, restecg, thalac, exang, oldpeak])
+        ]
 
-                # Lakukan prediksi dan tambahkan hasilnya ke dalam DataFrame result_df
-                prediction_tab2 = model.predict(data_tab2_normalized)
-                result_df = result_df.append({"Prediction Value": prediction_tab2[0], "Prediction Result": "Healthy" if prediction_tab2[0] == 0 else "Unhealthy"}, ignore_index=True)
-            
-            combined_df = pd.concat([uploaded_df, result_df], axis=1)
-            # Menampilkan DataFrame yang diunggah dan hasil prediksinya
-            st.dataframe(combined_df)
-        else:
-            st.warning("Uploaded DataFrame is empty.")
+        # Reshape the normalized inputs into a DataFrame
+        datauser_normalized = pd.DataFrame([normalized_inputs], columns=["Age", "Sex", "ChestPainType", "Trestbps", "Chol", "Fbs", "Restecg", "Thalac", "Exang", "OldPeak"])
+        
+        #ini untuk triger ketika tombol predik dipencet
+        if predict_button:
+            st.write(datauser)
+            # Now, you can use normalized_inputs for prediction
+            prediction = model.predict(datauser_normalized)
+
+            if prediction[0] == 0:
+                st.success("👍 You are Healthy")
+            else:
+                st.error(" 💔 You are diagnosed with heart disease")
+
+    with tab2:
+        #ini untuk multi prediksi
+        #upload csv
+        uploaded_df = st.file_uploader("Upload a CSV file", type='csv')
+
+    # Normalisasi input di dalam tab 2
+        if uploaded_df:
+            # Menggunakan st.file_uploader untuk mengunggah file CSV
+            uploaded_df = pd.read_csv(uploaded_df)
+
+            # Load JSON values from file
+            with open("min_max_values.json", "r") as json_file:
+                min_max_values_json = json.load(json_file)
+
+            # Ensure that the DataFrame is not empty before attempting to iterate
+            if not uploaded_df.empty:
+                # Iterate through each column in the uploaded DataFrame
+                for column in uploaded_df.columns:
+                    # Check if the column is present in min_max_values_json
+                    if column in min_max_values_json:
+                        # Normalize the column values
+                        uploaded_df[column] = (uploaded_df[column] - min_max_values_json[column]["min"]) / (min_max_values_json[column]["max"] - min_max_values_json[column]["min"])
+
+                # Convert specific columns to float64
+                float_columns = ["trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak"]
+                uploaded_df[float_columns] = uploaded_df[float_columns].astype(float)
+
+                # Membuat DataFrame untuk hasil prediksi
+                result_df = pd.DataFrame(columns=["Prediction Value", "Prediction Result"])
+
+                # Iterasi melalui setiap baris di DataFrame yang diunggah
+                for index, row in uploaded_df.iterrows():
+                    # Buat DataFrame untuk satu baris hasil prediksi
+                    data_tab2_normalized = pd.DataFrame([row], columns=uploaded_df.columns)
+
+                    # Lakukan prediksi dan tambahkan hasilnya ke dalam DataFrame result_df
+                    prediction_tab2 = model.predict(data_tab2_normalized)
+                    result_df = result_df.append({"Prediction Value": prediction_tab2[0], "Prediction Result": "Healthy" if prediction_tab2[0] == 0 else "Unhealthy"}, ignore_index=True)
+                
+                combined_df = pd.concat([uploaded_df, result_df], axis=1)
+                # Menampilkan DataFrame yang diunggah dan hasil prediksinya
+                st.dataframe(combined_df)
+            else:
+                st.warning("Uploaded DataFrame is empty.")
 

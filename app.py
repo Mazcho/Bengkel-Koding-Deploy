0c573d0018faff2f4c3e7cc4e77716157cdb8e1a
Mazcho <putrantonicholaus@gmail.com>
2024-01-05 16:44:03 +0700

    update model dan deploymnet

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import itertools
+# import itertools
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import accuracy_score
 import streamlit as st
-import time
+# import time
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
